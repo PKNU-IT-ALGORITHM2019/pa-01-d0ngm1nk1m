1927fa7dbda52c10d9524a8e2daef352a9d75b2f
d0ngm1nk1m <dmk218@naver.com>
2019-03-09 21:03:38 +0900

    second change

--- Work01.java
+++ Work01.java
@@ -10,15 +10,17 @@ public class Work01 {
     static String command;
     static String word;
     static int count;
-    static String []main;
+    static String []voca;
+    static String []part;
     static String []sub;
+    static String buffer;
     
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         
-        main = new String [Max];
+        voca = new String [Max];
+        part = new String [Max];
         sub = new String [Max];
-        int n = 0;
         
         while(true){
             
@@ -35,7 +37,8 @@ public class Work01 {
             }
             else if(command.equals("find")) {
                 word = keyboard.next();
-                find(word);
+                int num = find(0,count-1,word);
+                System.out.println(voca[num]);
             }
             else if(command.equals("exit"))
                 break;
@@ -48,8 +51,23 @@ public class Work01 {
             Scanner    inFile = new Scanner(new File(word2));
             
             while(inFile.hasNext()) { // detect End of File
-                main[count] = inFile.next();
-                sub[count] = inFile.nextLine();
+                buffer = inFile.nextLine();
+                int len = buffer.length();
+                int num1 = buffer.indexOf("(");
+                int num2 = buffer.indexOf(")");
+                
+                if(len == num2 + 1) {
+                    voca[count] = buffer.substring(0);
+                }
+                else {
+                    voca[count] = buffer.substring(0,num1-1);
+                    part[count] = buffer.substring(num1,num2+1);
+                    sub[count] = buffer.substring(num2+2);
+                }
+                
+                if(inFile.hasNext()) {
+                    buffer = inFile.nextLine();
+                }
                 count++;
             }
             
@@ -61,11 +79,20 @@ public class Work01 {
         
     }
     
-    public static void find(String word2) {
+    public static int find(int begin, int end, String word2) {
         
-        
-        
-        
+        int middle = (begin + end)/2;
+        if(begin <= end) {
+            return middle;
+            // System.out.println("Found " + num + " items.");
+        }
+        if(voca[middle].equals(word2))
+            return middle;
+        else if(voca[middle].compareTo(word2) < 0)
+            find(middle + 1, end, word2);
+        else
+            find(begin, middle - 1, word2);
+        return -1;
         
     }
     
